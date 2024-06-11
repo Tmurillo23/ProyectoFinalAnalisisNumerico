7e5a2deca3c0e0d1aa09ade9d3d2f8d50c648f00
Tatiana <tatana442015@hotmail.com>
2024-06-11 15:36:31 -0500

    Actualización módulo de serie

--- Modulos/serie.py
+++ Modulos/serie.py
@@ -1,6 +1,7 @@
 from sympy import *
 import numpy as np
 from math import factorial
+import matplotlib.pyplot as plt
 #--------------------------
 x = symbols('x')
 #--------------
@@ -28,7 +29,7 @@ def S_taylor(f,x0,n):
         dfx0 = df.subs(x,x0)
         p += dfx0*(x-x0)**k/factorial(k)
     return p
-        
+
     
 """
 ###
@@ -47,4 +48,20 @@ def cota_t(f,x0,xp,n):
     Mc = np.max(np.abs(df(u)))
     return Mc*np.abs((xp-x0)**(n+1)/factorial(n+1))
 
+
+"""
+###
+
+Grafica del polinomio vs la función
+
+
+"""
+
+def grafica_polinomio(f,x0,n):
+    w = np.linspace(x0,1,100)
+    P = S_taylor(f,x0,n)
+    P = lambdify(x,P)
+    F = lambdify(x,f)
+    plt.plot(w,P(w),label = f'Polinomio de grado {n}')
+    plt.plot(w, F(w), 'r', label='Funcion')
 
